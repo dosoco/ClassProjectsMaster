0dd3051486d64397dffac4693da8bdd86fbcb94f
kylexrose@gmail.com <kylexrose@gmail.com>
2023-11-08 12:00:26 -0500

    backend changes

---
+++ Backend/week01/JSReview/functions.js
@@ -0,0 +1,61 @@
+const returnEmptyString = function () {
+    return ""
+};
+
+const returnZeroNumber = function () {return 0};
+
+const returnEmptyArray = function () {};
+
+const returnEmptyObject = function () {};
+
+const returnAbcString = function () {};
+
+const returnSumOfTen = function () {};
+
+const subtraction = function () {};
+
+const multiplication = function () {};
+
+const division = function () {};
+
+const returnArray = function () {};
+
+const returnFirstIndex = function () {};
+
+const returnSecondIndex = function () {};
+
+const returnArrayLength = function () {};
+
+const arraySum = function () {};
+
+const arraySubtraction = function () {};
+
+const multiplicationArray = function () {};
+
+const divisionArray = function () {};
+
+const oddArray = function () {};
+
+const evenArray = function () {};
+
+module.exports = {
+returnEmptyString,
+returnZeroNumber,
+returnEmptyArray,
+returnEmptyObject,
+returnAbcString,
+returnSumOfTen,
+subtraction,
+multiplication,
+division,
+returnArray,
+returnFirstIndex,
+returnSecondIndex,
+returnArrayLength,
+arraySum,
+arraySubtraction,
+multiplicationArray,
+divisionArray,
+oddArray,
+evenArray
+}
---
+++ Backend/week01/callMeBacks/functions.js
@@ -0,0 +1,52 @@
+const identity = function () {
+
+};
+
+const first = function () {
+  
+};
+  
+const last = function () {
+  
+};
+
+const each = function () {
+  
+};
+
+const indexOf = function () {
+  
+};
+
+const map = function () {
+
+}
+
+const filter = ()=>{
+
+}
+
+const reject = () =>{
+
+}
+
+const uniq = () =>{
+
+}
+
+const reduce = () =>{
+
+}
+
+module.exports = {
+  identity,
+  first,
+  last,
+  each,
+  indexOf,
+  map,
+  filter, 
+  reject, 
+  uniq, 
+  reduce
+};
